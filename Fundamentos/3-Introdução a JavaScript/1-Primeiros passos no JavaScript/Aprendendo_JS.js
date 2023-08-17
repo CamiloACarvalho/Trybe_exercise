ff73287f394f323b6793ed43f42c736176d7786d
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2023-08-17 20:26:28 -0300

    Variáveis

---
+++ Fundamentos/3-Introdução a JavaScript/1-Primeiros passos no JavaScript/Aprendendo_JS.js
@@ -0,0 +1,13 @@
+// const -> são variáveis que são constantes e não podem ser atribuido outros valores para essa constante
+// let -> são variáveis que permitem alteração de seus atributos. Elas recebem os últimos valores
+
+//Tipo primitivo -> são tipos de de variáveis que podemos ter na programação. São elas:
+//No java script tem tipagem dinâmica, logo não precisa declarar se é int, float, char...
+//Typeof é uma forma de retonar o tipo de variável. Ex:
+
+let valor = 30; //number
+let pi = 3.1415; //number
+let msg = "hello world"; //string
+let boolean = true;
+
+console.log(typeof valor, typeof pi, typeof msg, typeof boolean);
