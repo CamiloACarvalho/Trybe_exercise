e986ed0cbfd0bc3281a4f90dc6a07ee3b2de0517
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2023-08-18 23:08:11 -0300

    Tentando resolver o problema da gerador de número da megea sena

--- Fundamentos/3-Introdução a JavaScript/2-Arrays e estruturas de repetição/Arrays e Estruturas de repetição.js
+++ Fundamentos/3-Introdução a JavaScript/2-Arrays e estruturas de repetição/Arrays e Estruturas de repetição.js
@@ -27,3 +27,36 @@ console.log(`a soma de 2+2 = ${2+2}`);
 console.log(`multiplicação de 3x5 = ${3*5}`);
 
 //Essa é o template literals
+
+//Número para jogar na mega sena
+let megaSena = [];
+let random;
+let randomNumber
+let numerosRepetidos = [];
+
+
+for(let num =0 ; num < 5; num +=1){
+    // Gera um número randômico entre 0 (inclusive) e 1 (exclusive)
+    random = Math.random();
+    // Gera um número randômico entre 1 e 60 (ambos inclusive)
+    randomNumber = Math.floor(random * 60) + 1;
+    //Armazena no Array megaSena e verifica se tem número igual
+    megaSena.push(randomNumber);    
+}
+
+console.log(megaSena);
+
+for(let k=0; k<megaSena.length; k+=1){
+
+    for(let j=k+1; j<megaSena.length; j+=1){
+
+        if(megaSena[k] === megaSena [j]){
+
+            numerosRepetidos.push(megaSena[k]);
+
+            megaSena.splice(k, 1); 
+        }
+    }
+}
+console.log(numerosRepetidos);
+console.log(megaSena);
