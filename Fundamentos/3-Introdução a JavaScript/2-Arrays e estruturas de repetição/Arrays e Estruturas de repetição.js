9f6532274ad1cdab805d432f3ebb2767f457b102
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2023-08-20 09:36:38 -0300

    terminei de fazer o gerador de números aleatórios da megasena. Pensando se na hora de imprimir ou ordeno os números do maior para o menor

--- Fundamentos/3-Introdução a JavaScript/2-Arrays e estruturas de repetição/Arrays e Estruturas de repetição.js
+++ Fundamentos/3-Introdução a JavaScript/2-Arrays e estruturas de repetição/Arrays e Estruturas de repetição.js
@@ -35,28 +35,37 @@ let randomNumber
 let numerosRepetidos = [];
 
 
-for(let num =0 ; num < 5; num +=1){
+for(let num =0 ; num <= 5; num +=1){
     // Gera um número randômico entre 0 (inclusive) e 1 (exclusive)
     random = Math.random();
     // Gera um número randômico entre 1 e 60 (ambos inclusive)
     randomNumber = Math.floor(random * 60) + 1;
     //Armazena no Array megaSena e verifica se tem número igual
-    megaSena.push(randomNumber);    
-}
+    megaSena.push(randomNumber); 
 
-console.log(megaSena);
+    for(let k=0; k<megaSena.length; k+=1){
 
-for(let k=0; k<megaSena.length; k+=1){
+        for(let j=k+1; j<megaSena.length; j+=1){
+    
+            if(megaSena[k] === megaSena [j]){
+    
+                numerosRepetidos.push(megaSena[k]);
+    
+                megaSena.splice(k, 1); 
 
-    for(let j=k+1; j<megaSena.length; j+=1){
+                random = Math.random();
+            
+                randomNumber = Math.floor(random * 60) + 1;
+              
+                megaSena.push(randomNumber); 
 
-        if(megaSena[k] === megaSena [j]){
-
-            numerosRepetidos.push(megaSena[k]);
-
-            megaSena.splice(k, 1); 
+            }
         }
     }
 }
-console.log(numerosRepetidos);
+
 console.log(megaSena);
+
+
+// console.log(numerosRepetidos);
+// console.log(megaSena);
