6e916cba7d6e776bcd74b2bb91586b9dbb6a40d4
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2023-08-30 21:38:09 -0300

    finalizado

--- Fundamentos/4-JavaScript : DOM, Eventos e Web Storage/03-JavaScript - Eventos/script.js
+++ Fundamentos/4-JavaScript : DOM, Eventos e Web Storage/03-JavaScript - Eventos/script.js
@@ -1,12 +1,50 @@
+function gerarNumeroAleatorio () {
+    return Math.random() * 200;
+};
+
+const resetGame = () => {
+    const player1 = document.getElementById('player1');
+    const player2 = document.getElementById('player2');
+
+    player1.style.marginLeft = 0;
+    player1.style.marginLeft = 0;
+
+    player1.style.backgroundImage = `url(/files/selectPlayer.png)`;
+    player2.style.backgroundImage = `url(/files/selectPlayer.png)`;
+};
+
 window.onload = () => {
-    const starBtn = document.getElementById('start-race-btn'); //captura o botão de start
+    const startBtn = document.getElementById('start-race-btn'); //captura o botão de start
     const resetbtn = document.getElementById('reset-race-btn');
     const player1 = document.getElementById('player1');
     const player2 = document.getElementById('player2');
     const audioWinner = document.getElementById('audioWinner');
 
-    const containerPlayers = document.querySelectorAll('.car-section')[0];
+    player1.style.marginLeft = 0;
+    player2.style.marginLeft = 0;
 
+    startBtn.addEventListener('click', () => {
+        player1.style.marginLeft = (parseInt(player1.style.marginLeft) + gerarNumeroAleatorio() + 'px');
+        player2.style.marginLeft = (parseInt(player2.style.marginLeft) + gerarNumeroAleatorio() + 'px');
+
+        const player1Win = parseInt(player1.style.marginLeft) > window.innerWidth;
+        const player2Win = parseInt(player1.style.marginLeft) > window.innerWidth;
+
+        if (player1Win){
+            alert ('PLAYER 1 VENCEU'); //Isso não é usado
+            audioWinner.play();
+            audioWinner.volume = 0.2;
+        }else if (player2Win){
+            alert ('PLAYER 2 VENCEU'); 
+            audioWinner.play();
+            audioWinner.volume = 0.2;
+        }
+
+    });
+
+    resetbtn.addEventListener('click', resetGame);
+
+    const containerPlayers = document.querySelectorAll('.car-section')[0];
     containerPlayers.addEventLister('click', (event) => { //o event.target indica o elemento que vc clicar. é uma forma de acessar o elemento que vc clicou
         const selected = document.querySelector('.selected');
         if (selected) {
