98f79532c61141353f56d069a70ff94618193a8c
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2023-09-07 11:14:30 -0300

    Não consegui criar o option através do DOM

--- Fundamentos/5-HTML e CSS: Forms, Flexbox e Responsivo/02-Bibliotecas JavaScript e Frameworks CSS/script.js
+++ Fundamentos/5-HTML e CSS: Forms, Flexbox e Responsivo/02-Bibliotecas JavaScript e Frameworks CSS/script.js
@@ -1,67 +1,3 @@
-window.onload = () => {
-    // CAPTURA DOS ELEMENTOS
-    const form = document.getElementById('form');
-    const weightMarmita = document.getElementById('tamanho');
-    const eggs = document.getElementById('ovo');
-    const ticket = document.querySelector('.ticket');
-    const orderList = document.getElementById('orderList');
-
-    // CRIA OPTIONS COM TODAS AS MARMITAS DISPONÍVEIS NO OBJETO FOODS
-    const getItemsMarmita = (data) => {
-        const weightMarmita = document.getElementById('tamanho');
-        for (let element of data.items) {
-            const newOption = document.createElement('option');
-            newOption.innerText = element.name;
-            weightMarmita.appendChild(newOption);//é uma forma de deixar a página dinâmica. Vc faz um dropdown através do DOM. Vc busca no JSON e coloca ele no HTML
-        }
-    };
-
-    // CAPTURA OS VALORES DOS INPUTS E MOSTRA A NOTA FISCAL.
-    form.addEventListener('submit', (event) => {
-        // const checkBox = document.getElementById('termos-uso');
-        // if (!checkBox.checked) {
-        event.preventDefault();
-        // }
-
-
-        const allEaseinputs = document.querySelectorAll('.my-inputs');
-        const options = ["Nome:", "E-mail:", "Tam. Marmita:", "Qtd Ovos:", "Comentários:"];
-        for (let index = 0; index < allEaseinputs.length; index += 1) {
-            const element = allEaseinputs[index];
-            if (options[index] === "Tam. Marmita:") {
-                getOrderList(`${options[index]} ${findMarmita(foods)}`);
-            } else {
-                getOrderList(`${options[index]} ${element.value}`);
-            }
-        }
-    });
-
-    // EXIBE OS ITEMS DO PEDIDO MAIS O VALOR TOTAL
-    const getOrderList = (inputValue) => {
-        const newElement = document.createElement('li');
-        newElement.innerText = inputValue;
-        orderList.appendChild(newElement);
-        // findMarmita(foods)
-        ticket.style.display = 'block';
-    };
-
-    // ENCONTRA A MARMITA DE ACORDO COM A OPÇÃO DO SELECT PARA 
-    // DESCOBRIR O PREÇO.
-    const findMarmita = (data) => {
-        let marmita;
-        for (let index = 0; index < data.items.length; index += 1) {
-            if (weightMarmita.value === data.items[index].name) {
-                marmita = data.items[index].name;
-                break;
-            }
-        }
-        return marmita;
-    };
-
-    // CHAMA A FUNÇÃO PARA POPULAR O SELECT COM AS OPÇÕES DE MARMITA
-    getItemsMarmita(foods);
-};
-
 // JSON
 const foods = {
     items: [
@@ -75,11 +11,75 @@ const foods = {
         },
         {
             name: 'Marmitão - 1Kg',
-            price: 27.90
+            price: 27.90,
         },
         {
             name: 'Marmitãozão - 2Kg',
-            price: 39.90
+            price: 39.90,
         },
-    ]
+    ],
+};
+
+// CRIA OPTIONS COM TODAS AS MARMITAS DISPONÍVEIS NO OBJETO FOODS
+const getItemsMarmita = (foods) => {
+    const weightMarmita = document.getElementById('tamanho');
+    for (let index = 0; index < foods.items.length; index += 1) {
+        const newOption = document.createElement('option');
+        newOption.innerText = foods.items[index].name;
+        weightMarmita.appendChild(newOption);
+    }
+};
+
+// EXIBE OS ITEMS DO PEDIDO MAIS O VALOR TOTAL
+const getOrderList = (inputValue) => {
+    const orderList = document.getElementById('orderList');
+    const ticket = document.querySelector('.ticket');
+
+    const newElement = document.createElement('li');
+    newElement.innerText = inputValue;
+    orderList.appendChild(newElement);
+    // findMarmita(foods)
+    ticket.style.display = 'block';
+};
+
+// ENCONTRA A MARMITA DE ACORDO COM A OPÇÃO DO SELECT PARA 
+// DESCOBRIR O PREÇO.
+const findMarmita = (foods) => {
+    const weightMarmita = document.getElementById('tamanho');
+
+    let marmita;
+    for (let index = 0; index < foods.items.length; index += 1) {
+        if (weightMarmita.value === foods.items[index].name) {
+            marmita = foods.items[index].name;
+            break;
+        }
+    }
+    return marmita;
+};
+
+window.onload = () => {
+    // CAPTURA DOS ELEMENTOS
+    const form = document.getElementById('form');
+
+    // CHAMA A FUNÇÃO PARA POPULAR O SELECT COM AS OPÇÕES DE MARMITA
+    getItemsMarmita();
+
+    // CAPTURA OS VALORES DOS INPUTS E MOSTRA A NOTA FISCAL.
+    form.addEventListener('submit', (event) => {
+        // const checkBox = document.getElementById('termos-uso');
+        // if (!checkBox.checked) {
+            event.preventDefault();
+        // }
+
+        const allEaseinputs = document.querySelectorAll('.my-inputs');
+        const options = ['Nome:', 'E-mail:', 'Tam. Marmita:', 'Qtd Ovos:', 'Comentários:'];
+        for (let index = 0; index < allEaseinputs.length; index += 1) {
+            const element = allEaseinputs[index];
+            if (options[index] === 'Tam. Marmita:') {
+                getOrderList(`${options[index]} ${findMarmita(foods)}`);
+            } else {
+                getOrderList(`${options[index]} ${element.value}`);
+            }
+        }
+    });
 };
