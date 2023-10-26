892bd35b341c6174bec8b909529a2c081a43fec9
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2023-10-26 20:04:39 -0300

    Finalizando os arquivos para a aula de hoje

---
+++ Front_End/2-Introdução ao React/6-Formulários/exercicio-aula-2.6/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>Work in progress</div>
+      <h1>Desenvolvimento Web</h1>
+    </>
+  );
+}
+
+export default App;
