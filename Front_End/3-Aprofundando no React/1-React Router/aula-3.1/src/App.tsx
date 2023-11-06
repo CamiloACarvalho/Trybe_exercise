84c01115812b37b8ce6006ed1d87f8a219738323
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2023-11-06 13:53:30 -0300

    Atualizando conteudo do aula 3.1

--- Front_End/3-Aprofundando no React/1-React Router/aula-3.1/src/App.tsx
+++ Front_End/3-Aprofundando no React/1-React Router/aula-3.1/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import UserProfile from './pages/UserProfile';
+import NotFound from './pages/NotFound';
+import Layout from './components/Layout';
+
+/*
+  instalou a lib react-router-dom
+  englobou a nossa aplicação com a BrowserRouter
+  criou a primeira rota com os componentes Routes e Route
+  criou a navegação entre as páginas com o componente Link
+  criou a página NotFound com a rota * <Route path="*" element={ <NotFound /> } />
+  criou uma rota dinâmica usando <Route path="/user/:username" element={ <UserProfile /> } />
+  recuperou o parametro da rota com o hook useParams
+  fez uma navegação dinâmica com o hook useNavigate
+  criou uma rota aninhada para ter um layout específico para um grupo de rotas
+  usou o componente Outlet para renderizar as rotas filhas
+*/
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  /*
+    <Layout>
+      <HomePage />
+    </Layout>
+  */
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={ <Layout /> }>
+        <Route path="/" element={ <HomePage /> } />
+        <Route path="/user/:username" element={ <UserProfile /> } />
+      </Route>
+
+      <Route path="*" element={ <NotFound /> } />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
