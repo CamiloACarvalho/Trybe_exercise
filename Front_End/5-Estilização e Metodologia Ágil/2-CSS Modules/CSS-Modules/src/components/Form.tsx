61b896132d2495dfe29766d449ab50396b6d5b10
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2023-11-22 14:24:53 -0300

    up

--- Front_End/5-Estilização e Metodologia Ágil/2-CSS Modules/CSS-Modules/src/components/Form.tsx
+++ Front_End/5-Estilização e Metodologia Ágil/2-CSS Modules/CSS-Modules/src/components/Form.tsx
@@ -1,7 +1,42 @@
-const Form = () => {
-  return(
-
+function Form() {
+  return (
+    <>
+      <div className="aside">
+        <p>test</p>
+      </div>
+      <div className="container">
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Insira o seu login"
+            aria-label="Insira o seu login"
+            aria-describedby="basic-addon2"
+          />
+          <span
+            className="input-group-text"
+            id="basic-addon2"
+          >
+            @example.com
+          </span>
+        </div>
+        <div className="input-group mb-3">
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Insira sua senha"
+            aria-label="Insira sua senha"
+            aria-describedby="basic-addon2"
+          />
+        </div>
+        <button
+          className="verified"
+          type="submit"
+        >
+          Confirmar
+        </button>
+      </div>
+    </>
   );
 }
-
 export default Form;
