9906cca3a193c02bf51b13a24323c9fda6a5ac90
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2023-11-22 18:58:08 -0300

    ainda estilizando

--- Front_End/5-Estilização e Metodologia Ágil/2-CSS Modules/CSS-Modules/src/components/Form.tsx
+++ Front_End/5-Estilização e Metodologia Ágil/2-CSS Modules/CSS-Modules/src/components/Form.tsx
@@ -1,10 +1,55 @@
 import sytle from './form.module.css';
+import linkedin from './public/linkedin.svg';
 
 function Form() {
   return (
     <>
       <div className={ sytle.aside }>
-        <p>test</p>
+        <a
+          href="https://www.linkedin.com/in/camiloaugustocarvalho"
+        >
+          <img
+            src="./public/linkedin.svg"
+            className={ sytle.linkedin }
+            alt="logo do LinkedIn"
+          />
+        </a>
+        <a
+          href="https://github.com/CamiloACarvalho"
+        >
+          <img
+            src="./public/github.svg"
+            className={ sytle.github }
+            alt="logo do github"
+          />
+        </a>
+        <a
+          href="https://trybecourse.slack.com/team/U05LEU20N7J"
+        >
+          <img
+            src="./public/slack.svg"
+            className={ sytle.slack }
+            alt="logo do slack"
+          />
+        </a>
+        <a
+          href="https://instagram.com/camilocarv_?igshid=YTQwZjQ0NmI0OA%3D%3D&utm_source=qr"
+        >
+          <img
+            src="./public/instagram.svg"
+            className={ sytle.instagram }
+            alt="logo do instagram"
+          />
+        </a>
+        <a
+          href="https://discord.gg/XCqRygW7"
+        >
+          <img
+            src="./public/discord.svg"
+            className={ sytle.discord }
+            alt="logo do discord"
+          />
+        </a>
       </div>
       <div className={ sytle.container }>
         <div
