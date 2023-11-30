d0021e8ae2ae4cf19294d90e19cf3756d44a2849
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2023-11-30 14:29:55 -0300

    Falta arquivos

---
+++ Front_End/5-Estilização e Metodologia Ágil/3-Styled Components/style-components/src/App.tsx
@@ -0,0 +1,97 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import ViewTasks from './components/view-tasks/view-tasks';
+import ListTasks from './components/list-tasks/list-tasks';
+
+import { Task } from './types';
+import GlobalStyles from './styles/global-styles';
+import { ThemeProvider } from 'styled-components';
+import dark from './styles/themes/dark';
+import light from './styles/themes/light';
+
+function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [allTasks, setAllTasks] = useState<Task[]>([]);
+  const completedTasks = allTasks.filter((task) => task.completed);
+  const activeTasks = allTasks.filter((task) => !task.completed);
+
+  const changeTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  }
+
+  const toggleStatusTask = (currentTask: Task) => {
+    const newTasks = allTasks.map((task) => {
+      if (task.id === currentTask.id) {
+        return ({
+          ...task,
+          completed: !task.completed,
+        });
+      }
+      return task;
+    });
+
+    setAllTasks(newTasks);
+  };
+
+  const addNewTask = (newTask: Task) => {
+    setAllTasks((prevAllTasks) => [...prevAllTasks, newTask]);
+  };
+
+  const deleteTask = (taskToDelete:Task) => {
+    setAllTasks((prevAllTasks) => prevAllTasks
+      .filter((task) => task.id !== taskToDelete.id));
+  };
+
+  const clearCompletedTasks = () => {
+    setAllTasks((prevAllTasks) => prevAllTasks.filter((task) => !task.completed));
+  };
+
+  return (
+    <BrowserRouter>
+    <ThemeProvider theme={ isDarkMode ? dark : light }>
+      <GlobalStyles />
+        <Routes>
+          <Route
+            path="/"
+            element={ <ViewTasks
+              itemsLeftToCompleted={ allTasks.length - completedTasks.length }
+              addNewTask={ addNewTask }
+              clearCompletedTasks={ clearCompletedTasks }
+              isDarkMode={ isDarkMode }
+              changeTheme={ changeTheme }
+            /> }
+          >
+            <Route
+              index
+              element={ <ListTasks
+                tasks={ allTasks }
+                toggleStatusTask={ toggleStatusTask }
+                deleteTask={ deleteTask }
+              /> }
+            />
+            <Route
+              path="/active"
+              element={ <ListTasks
+                tasks={ activeTasks }
+                toggleStatusTask={ toggleStatusTask }
+                deleteTask={ deleteTask }
+              /> }
+            />
+            <Route
+              path="/completed"
+              element={ <ListTasks
+                tasks={ completedTasks }
+                toggleStatusTask={ toggleStatusTask }
+                deleteTask={ deleteTask }
+              /> }
+            />
+          </Route>
+        </Routes>
+    </ThemeProvider>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
